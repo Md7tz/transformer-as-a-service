b615c757be24aaee04dd299cc48be000a6dee9d0
Medhat Usama <medhatusama@gmail.com>
2024-04-20 15:53:38 +0800

    TS-6 feat: app layout sidebar and header

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,14 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import {SessionProvider} from "next-auth/react"
-import {Session} from "next-auth";
+import { SessionProvider } from "next-auth/react"
+import { Session } from "next-auth";
+import Layout from "@/components/Layout";
 
-export default function App({Component, pageProps}: AppProps<{
+export default function App({ Component, pageProps }: AppProps<{
     session: Session;
 }>) {
     return (
         <SessionProvider session={pageProps.session}>
-            <Component {...pageProps} />
+            <Layout view="playground" >
+                <Component {...pageProps} />
+            </Layout >
         </SessionProvider>
     )
 }
